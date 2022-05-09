b0c19a022efed825afc3c5506219da2696e570f1
Colleen Murray <colleenkhm@gmail.com>
2022-05-08 22:19:10 -0500

    product and category routes updated

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,25 +4,63 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
+  Category.findAll()
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findOne()
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_text: req.body.category_text,
+    // uncertain if these are correct
+    // category_id: req.body.category_id,
+    // product_id: req.body.product_id
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: `No category found with this id!` });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
---
+++ README.md
@@ -0,0 +1,14 @@
+# E-Commerce Back End Application
+
+## Description
+An application that allows the client to create and edit databases to store information on various products and categories.
+
+## Built With
+- JavaScript
+- Node.js
+- Express.js
+- MySQL
+- Sequelize
+
+## Contributing
+Made by Colleen Murray
