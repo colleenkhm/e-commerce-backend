285b495dfdb318423c6501c54e05b5159f3f1c12
Colleen Murray <colleenkhm@gmail.com>
2022-05-08 21:16:41 -0500

    initialized category db

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,4 +1,6 @@
+const { NULL } = require('mysql/lib/protocol/constants/types');
 const { Model, DataTypes } = require('sequelize');
+const { DataTypes, DataTypes } = require('sequelize/types');
 
 const sequelize = require('../config/connection.js');
 
@@ -6,7 +8,19 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+      },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
   },
   {
     sequelize,
